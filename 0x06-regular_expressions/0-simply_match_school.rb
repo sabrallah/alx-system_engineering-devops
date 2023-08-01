d5e0a2746bfce2552b0c310e4195d8f1331fee5f
sabrallah <sabrallah1991@gmail.com>
2023-08-01 05:59:56 -0700

    task0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,12 +1,3 @@
 #!/usr/bin/env ruby
 
-# Accept the argument passed to the script
-input = ARGV[0]
-
-# Use the regular expression to match "School"
-regex = /School/
-
-# Check if the input matches the regular expression
-if input.match?(regex)
-  puts input
-end
+puts ARGV[0].scan(/School/).join
