760aeb1bda766872dc6c3bd4f3c751f62faa0ff7
sabrallah <sabrallah1991@gmail.com>
2023-09-22 01:40:56 -0700

    task0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,14 @@
+# This Puppet manifest creates a file in /tmp with the following requirements:
+# * File path is /tmp/school
+# * File permission is 0744
+# * File owner is www-data
+# * File group is www-data
+# * File contains "I love Puppet"
+
+file { '/tmp/school':
+  ensure => present,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0744',
+  content => 'I love Puppet',
+}
