97d7432935781c21982151036537d2b4464e868c
sabrallah <sabrallah1991@gmail.com>
2023-09-22 02:22:44 -0700

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # Install Flask 2.1.0
-package { 'python3-pip':
-  ensure => present,
-}
 
-package { 'flask':
-  ensure => present,
-  provider => 'pip3',
-  version => '2.1.0',
+package { 'puppet-lint':
+   ensure   => '2.5.0',
+   provider => gem,
 }
