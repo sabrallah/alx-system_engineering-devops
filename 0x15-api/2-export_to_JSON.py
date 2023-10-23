aa200a43b58666e411328b114172079ecd7ac8e1
sabrallah <sabrallah1991@gmail.com>
2023-10-23 15:51:27 -0700

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script converts data to json """
+""" a script that converts data to json """
 
 import json
 import requests
@@ -7,15 +7,17 @@ import sys
 
 if __name__ == "__main__":
     userId = sys.argv[1]
-    # use one request to get todos of secific user
-    todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(userId))
+    # Divisez la ligne longue en plusieurs lignes
+    todos_url = (
+        'https://jsonplaceholder.typicode.com'
+        '/users/{}/todos'.format(userId)
+    )
+    todos = requests.get(todos_url)
 
-    # no need to do another request to get user
     name = todos.json()[0].get('username')
 
     taskList = []
 
-    # no need to check userId because we are already filtrate todos per userId
     for task in todos.json():
         taskDict = {
             "task": task.get('title'),
