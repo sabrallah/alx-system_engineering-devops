761e33e2613745e2c773dbc8f455a19a5288cac8
sabrallah <sabrallah1991@gmail.com>
2023-10-23 15:32:55 -0700

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-
-"""Script that uses JSONPlaceholder API to get information about employee"""
-
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
 if __name__ == "__main__":
-
     url = 'https://jsonplaceholder.typicode.com/'
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
 
-    print(f"Employee {json_o['name']} is done with tasks", end="")
+    user = requests.get(f'{url}users/{sys.argv[1]}').json()
+    print(f"Employee {user.get('name')} is done with tasks", end="")
 
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
+    tasks = requests.get(f'{url}todos?userId={sys.argv[1]}').json()
+    completed_tasks = [task for task in tasks if task.get('completed')]
 
-    completed_tasks = [task for task in tasks if task['completed']]
-
-    print("({}/{}):".format(len(completed_tasks), len(tasks)))
-
+    print(f"({len(completed_tasks)}/{len(tasks)}):")
     for task in completed_tasks:
-        print("\t{}".format(task['title']))
+        print(f"\t {task.get('title')}")
