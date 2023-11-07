1c3c0f12b105fb59e1ef8a962c443c91e681a6e0
sabrallah <sabrallah1991@gmail.com>
2023-11-07 13:15:26 -0800

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,14 +5,14 @@
 import requests
 
 
-def get_hot_posts(subreddit):
+def top_ten(subreddit):
     """
     Get 10 hot posts from a subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'user-agent': 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
- 
+
     if response.status_code != 200:
         print(None)
         return
