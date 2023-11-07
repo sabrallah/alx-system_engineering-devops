f6414f66220ee854b98fd5d1bd838dcbc9a0ba9b
sabrallah <sabrallah1991@gmail.com>
2023-11-07 12:59:26 -0800

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,11 +12,11 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'user-agent': 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
-    
+
     data = response.json().get("data")
     num_subs = data.get("subscribers")
-    
+
     return num_subs
