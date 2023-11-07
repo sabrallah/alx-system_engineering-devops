a0c6159c13aa339325a9b659d685b221f328d7ec
sabrallah <sabrallah1991@gmail.com>
2023-11-07 13:20:40 -0800

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """
-    Uses Reddit API to get 10 hot posts
+    Uses reddit API to get 10 hot posts
 """
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Get 10 hot posts from a subreddit
-    """
+    """Get 10 hot posts"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'user-agent': 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -18,5 +16,5 @@ def top_ten(subreddit):
         return
 
     data = response.json().get("data").get("children")
-    top_10_posts = "\n".join([post.get("data").get("title") for post in data])
-    print(top_10_posts, end="")
+    top_10_posts = "\n".join(post.get("data").get("title") for post in data)
+    print(top_10_posts)
