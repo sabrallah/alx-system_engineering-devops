d31e2bb7712524187c66971f0aa536a55d8ae33f
sabrallah <sabrallah1991@gmail.com>
2023-11-07 16:08:35 -0800

    task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
-# Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
-
-exec { 'fix-wordpress':
-  command => '/bin/sed -i "s/short_open_tag = Off/short_open_tag = On/g" /etc/php5/apache2/php.ini',
-  path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-  require => Package['apache2'],
+# setting fixed at Wordpress
+exec { 'settingPress':
+  command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+  provider => shell,
 }
